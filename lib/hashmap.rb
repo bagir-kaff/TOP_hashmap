4dad2d02c7ca898178b2ccfd0304efa7ca05944e
bagir-kaff <trexhilang@gmail.com>
2024-07-30 16:41:04 +0330

    Start from scratch

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,9 +1,18 @@
-def hash(key)
-  hash_code = 0
-  prime_number = 31
+require_relative 'linked_list'
 
-  key.each_char { |char| hash_code = prime_number * hash_code + char.ord}
-  hash_code
-end
+class HashMap
+  attr_reader :number_of_items
+  def initialize
+    @bucket = Array.new(16)
+    @load_factor = 0.75
+    @mod = 16
+    @number_of_items = 0
+  end
 
+  def hash(key,whatever=nil)
+    hash_code = 0
+    prime_number = 31
 
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord}
+    return hash_code
+  end
